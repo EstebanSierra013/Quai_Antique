aced524889c22ace3d110578df21b7cb8927f072
EstebanSierra013 <anesierrasa@unal.edu.co>
2024-09-05 12:39:28 +0200

    Init Spring projet

---
+++ src/main/java/com/application/quai/QuaiAntiqueApplication.java
@@ -0,0 +1,13 @@
+package com.application.quai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaiAntiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuaiAntiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/quai/QuaiAntiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.quai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuaiAntiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
